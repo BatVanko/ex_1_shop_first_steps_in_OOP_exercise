36dd22d32536e96efc69dbd712a64769590e7d15
BatVanko <71530199+BatVanko@users.noreply.github.com>
2023-01-19 01:03:17 +0200

    Add files via upload

---
+++ ex_1_shop.py
@@ -0,0 +1,16 @@
+class Shop:
+    def __init__(self, name: str, items: list ):
+        self.items = items
+        self.name = name
+    def get_items_count(self):
+        return len(self.items)
+
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
+
+
+
+
+
+
